9e9595fa9c7cfd889151345bc29245b05fb69d14
xiluna <xilaluna2@gmail.com>
2020-10-02 20:53:04 -0700

    backwards func

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def get_file_lines(filename):
     return read_file
 
 
+def lines_printed_backwards(lines_list):
+    """
+    prints lines backwards with their associated line number
+    """
+
+    line_counter = len(lines_list) + 1
+    for line in reversed(lines_list):
+        line_counter -= 1
+        print(f'{line_counter} {line}')
+    return line
+
+
 # print(get_file_lines(poem))
 # lines_printed_backwards(get_file_lines(poem))
 # lines_printed_random(get_file_lines(poem))
